d8eab902c81fd46061b949ba3f002e71643c1fbf
Kamesh <kamesh231@gmail.com>
2018-09-06 17:56:01 +0530

    Initial Commit

---
+++ sentilyt/src/main/java/org/sentilyt/SentilytApplication.java
@@ -0,0 +1,12 @@
+package org.sentilyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentilytApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentilytApplication.class, args);
+	}
+}
